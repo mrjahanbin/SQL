377a56796996fbff94ce813390fabb5d39c75d8e
Mohammad Javad Jahanbin <mrjahanbin90@gmail.com>
2023-05-19 17:26:31 +0330

    init17 16

---
+++ SQLQuery003-Session 16.sql
@@ -0,0 +1,54 @@
+﻿--SQL Learning 16
+
+USE SampleDB
+
+--ایجاد جدول
+CREATE TABLE tbl1
+(
+code INT NOT NULL,
+family NVARCHAR(100 ) NULL
+)
+GO
+
+--مشاهده مشخصات جدول
+EXEC sp_help @objname = 'tbl1'
+GO
+
+--درج رکورد در جدول
+INSERT INTO dbo.tbl1(code,family)
+VALUES(1,N'صابری'),(2,N'x')
+go
+
+--مشاهده رکوردها در جدول
+SELECT * FROM dbo.tbl1
+GO
+
+
+--ایجاد جدول
+CREATE TABLE tbl2
+(
+code INT IDENTITY(1,1) NOT NULL,
+family VARCHAR(100 ) NULL
+)
+GO
+
+--درج رکورد در جدول
+INSERT INTO dbo.tbl2(code,family)
+VALUES(1,N'صابری'),(2,N'x')
+go
+
+--مشاهده رکوردها در جدول
+SELECT * FROM dbo.tbl2
+GO
+
+--اصلاح نوع داده یک فیلد
+ALTER TABLE dbo.tbl2
+ALTER COLUMN family NVARCHAR(10)
+
+--اضاف کردن فیلد به جدول
+ALTER TABLE dbo.tbl2
+ADD country NVARCHAR(50) NULL,
+city NVARCHAR(50)NULL
+go
+
+
---
+++ SQLQuery004-Session 17.sql
@@ -0,0 +1,88 @@
+﻿--SQL Learning 17
+
+USE SampleDB
+GO
+
+/*
+Constraintاضاف کردن
+به فیلد
+
+DontNull = اسم اون 
+Constraint
+هستش که میخواد ذخیره بشه
+*/
+ALTER TABLE  students --میگیم میخوایم جدول رو تغییر بدیم
+ADD CONSTRAINT DontNull --یک محدودیت جدید بساز و اسمش رو بهش دادیم
+DEFAULT (N'No') FOR LastName --توی این محدودیت بیا و برامون حالت دیفالت لست نیم رو به  مقداری که بهت گفتیم پر کن
+GO
+
+
+
+/*
+DEFAULT CONSTRAINT
+*/
+
+USE master
+GO
+-- بررسی جهت وجود دیتابیس و حذف آن 
+DROP DATABASE IF EXISTS sampleDB
+GO
+
+-- ایجاد دیتابیس
+CREATE DATABASE sampleDB 
+GO
+
+USE sampleDB;
+GO
+
+-- ایجاد جدول
+CREATE TABLE students
+(
+	nationalCode INT CONSTRAINT myConst1 DEFAULT 0,
+	family NVARCHAR(100) NOT NULL,
+	medical_status NVARCHAR(100) CONSTRAINT myConst2 DEFAULT N'ندارد',	
+	BLOOD_GROUP NVARCHAR(100) CONSTRAINT myConst3 DEFAULT N'OOOOO'
+)
+GO
+
+-- درج رکورد در جدول
+INSERT INTO students (family)
+	values (N'احمدی'),(N'راد'),(N'سعیدی')
+GO
+
+-- مشاهده رکوردها در جدول
+SELECT * FROM students
+GO
+
+-- های جدولCONSTRAINT مشاهده
+sp_helpCONSTRAINT 'students'
+GO
+
+/*
+در زمان اضافه کردن فیلد جدید به جدول موجودDEFAULT CONSTRAINT اعمال
+در این حالت میبینیم که اگر 
+WITH VALUES
+داشته باشه مقادیر قدیمی رو هم پر میکنه اگر نداشته باشه نال در نظر میگیره
+
+*/
+-- WITH VALUES با استفاده از
+ALTER TABLE students
+	ADD code1 INT DEFAULT 100000  WITH VALUES
+GO
+
+-- مشاهده رکوردها در جدول
+SELECT * FROM STUDENTS
+GO
+
+-- WITH VALUES بدون استفاده از
+ALTER TABLE students
+	ADD code2 INT DEFAULT 200000
+GO
+
+/*
+CONSTRAINT حذف
+*/
+
+ALTER TABLE students
+	DROP CONSTRAINT myConst1
+GO
